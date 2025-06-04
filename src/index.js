8792fb51fde88d644d2154434e2b502560d6a4c8
Your Name <your-email@example.com>
2025-06-04 20:29:10 +0530

    Fix build issues and update configurations

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import './index.css';
-=======
->>>>>>> 5e80e68620bf7ac6907819075b608ec2788afcff
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -17,8 +14,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-<<<<<<< HEAD
 reportWebVitals();
-=======
-reportWebVitals(); 
->>>>>>> 5e80e68620bf7ac6907819075b608ec2788afcff
